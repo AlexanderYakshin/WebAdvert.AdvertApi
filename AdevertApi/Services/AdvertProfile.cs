73783983bf8d8a674b07111cffdd8bfadc1a0ee0
camlang <camlang41@gmail.com>
2019-10-14 16:33:23 +0300

    Add models and controllers

---
+++ AdevertApi/Services/AdvertProfile.cs
@@ -0,0 +1,13 @@
+﻿using AdvertApi.Models;
+using AutoMapper;
+
+namespace AdevertApi.Services
+{
+	public class AdvertProfile : Profile
+	{
+		public AdvertProfile()
+		{
+			CreateMap<AdvertModel, AdvertDbModel>();
+		}
+	}
+}
