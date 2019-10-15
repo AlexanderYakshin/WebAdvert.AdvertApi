ac9614e63aa3f93c2ed15955b8e9b318e14c9960
camlang <camlang41@gmail.com>
2019-10-15 12:35:27 +0300

    Implement api for storing data

--- AdevertApi/Startup.cs
+++ AdevertApi/Startup.cs
@@ -1,11 +1,14 @@
+using AdevertApi.HealthChecks;
 using AdevertApi.Services;
 using AutoMapper;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
 using Microsoft.Extensions.Hosting;
 using System;
+using System.Collections.Generic;
 
 namespace AdevertApi
 {
@@ -25,7 +28,10 @@ namespace AdevertApi
 			//ServiceCollectionExtensions.AddAutoMapper(services);
 			services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 			services.AddTransient<IAdvertStorageService, DynamoDbAdvertStorage>();
+
 			services.AddControllers();
+			services.AddHealthChecks()
+				.AddCheck<StorageHealthCheck>("Storage", HealthStatus.Unhealthy, new List<string>(), new TimeSpan(0,1,0));
 		}
 
 		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -42,8 +48,10 @@ namespace AdevertApi
 
 			app.UseAuthorization();
 
+			//app.UseHealthChecks("/health");
 			app.UseEndpoints(endpoints =>
 			{
+				endpoints.MapHealthChecks("/health");
 				endpoints.MapControllers();
 			});
 		}
